8a6af7187eaa365dc4089ba6a516e03f69e984e5
parsec <beckmanseth@gmail.com>
2021-12-09 02:52:03 -0500

    added comments

--- 1-10/2/evenFibonacci.go
+++ 1-10/2/evenFibonacci.go
@@ -4,16 +4,16 @@ import "fmt"
 
 func main() {
 	var sum int
-	fib := []int{1, 1}
+	fib := []int{1, 1} // the start of the fibonacci sequence, our first two numbers
 	for {
-		fibNext := fib[0] + fib[1]
+		fibNext := fib[0] + fib[1] // add the last two indexes in the fibonacci sequence
 		if fibNext >= 4000000 {
 			break
 		}
-		if fibNext%2 == 0 {
+		if fibNext%2 == 0 { // check that it's divisible by 2 (even)
 			sum += fibNext
 		}
-		fib[0] = fib[1]
+		fib[0] = fib[1] // move values to the left for next numbers in sequence
 		fib[1] = fibNext
 	}
 	fmt.Println("The sum of even Fibonacci numbers below 4M is", sum)
